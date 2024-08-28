cb092c1f9ad965d132fabb430da7f43e651b1632
Den <gridden1.student@gmail.com>
2024-08-28 16:50:33 +0300

    Added encoder test example

--- prod2vec/model/encoder.py
+++ prod2vec/model/encoder.py
@@ -16,10 +16,12 @@ class ProdFeatureEncoder(nn.Module):
         self.model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
         self.fc = nn.Linear(self.config.bert_output_size, self.config.embedding_size)
         init.xavier_uniform_(self.fc.weight)
+        self.norm = nn.LayerNorm(self.config.embedding_size)
 
     def forward(self, text: str):
         t = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
         model_output = self.model(**{k: v.to(self.model.device) for k, v in t.items()})
         embedding = model_output.last_hidden_state[:, 0, :]
         embedding = self.fc(embedding)
+        norm_embedding
         return embedding[0]
