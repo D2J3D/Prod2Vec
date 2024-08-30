1183bf37b853bf96ad65d304d414887c812ec628
Den <gridden1.student@gmail.com>
2024-08-31 01:20:28 +0300

    fix: ProdFeaturesDataset class

--- prod2vec/ops/ProdFeaturesDataset.py
+++ prod2vec/ops/ProdFeaturesDataset.py
@@ -1,21 +1,51 @@
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 
 class ProdFeaturesDataset(Dataset):
-    def __init__(self, img_filepath, text_filepath, attr_filepath):
+    """
+    A PyTorch Dataset class for loading and transforming product features.
+    """
+
+    def __init__(self, img_filepath, text_filepath, attr_filepath, text_transform):
+        """
+        Parameters
+        ---------
+        :param img_filepath : str
+                Filepath to the parquet file containing image embeddings.
+        :param text_filepath : str
+                Filepath to the parquet file containing name and description data.
+                **This DataFrame must not contain any NaN values** and its index should be in ascending order.
+        :param attr_filepath : str
+                Filepath to the parquet file containing attribute data.
+                **This DataFrame must not contain any NaN values** and its index should be in ascending order.
+        :param text_transform : callable
+                A function that takes in a string and returns a transformed string.
+        """
         self.img_df = pd.read_parquet(img_filepath)
         self.name_descr_df = pd.read_parquet(text_filepath)
         self.attr_df = pd.read_parquet(attr_filepath)
+        self.text_transform = text_transform
+
+        # ensure everything is ok with passed parquet data
+        self.img_df = self.img_df.reset_index(drop=True)
+        self.name_descr_df = self.name_descr_df.reset_index(drop=True)
+        self.attr_df = self.attr_df.reset_index(drop=True)
+
+        self.name_descr_df = self.name_descr_df.fillna("")
+        self.attr_df = self.attr_df.fillna("")
 
     def __len__(self):
         return len(self.name_descr_df)
 
-    def __getitem__(self, variant_id):
-        main_img_emb = self.img_df.loc[self.img_df['variantid']
-                                       == variant_id, 'main_pic_embeddings_resnet_v1']
-        name, description = self.name_descr_df.loc[self.name_descr_df['variantid'] == variant_id, [
+    def __getitem__(self, idx):
+        main_img_emb = self.img_df.loc[idx, 'main_pic_embeddings_resnet_v1']
+        main_img_emb = torch.tensor(main_img_emb[0])
+        name, description = self.name_descr_df.loc[idx, [
             'name', 'description']]
-        attributes = self.attr_df.loc[self.attr_df['variantid']
-                                      == 'variantid', 'characteristic_attributes_mapping']
+        name, description = self.text_transform(
+            name), self.text_transform(description)
+        attributes = self.text_transform(
+            self.attr_df.loc[idx, 'characteristic_attributes_mapping'])
         return main_img_emb, name, description, attributes
