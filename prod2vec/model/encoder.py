ef86cf5c514aad848db73d43a9a4b91e7eccdcfc
Den <gridden1.student@gmail.com>
2024-08-30 15:38:47 +0300

    Docstrings for models were created

--- prod2vec/model/encoder.py
+++ prod2vec/model/encoder.py
@@ -1,16 +1,27 @@
 import torch
-import torch.nn as nn
-import torch.nn.init as init
-import torch.functional as F
+from torch import nn
+from torch.nn import init
 from transformers import AutoTokenizer, AutoModel
 
 
 class ProdFeatureEncoder(nn.Module):
+    """
+    Model for creating embeddings with pre-trained ruBERT-tiny BERT.
+
+    Attributes:
+        config (object): Configuration object containing model hyperparameters.
+        tokenizer (AutoTokenizer): Tokenizer instance for ruBERT-tiny.
+        model (AutoModel): Pre-trained ruBERT-tiny model instance.
+        fc (nn.Linear): Linear layer for dimensionality reduction.
+    """
     def __init__(self, config):
-        '''
-                Model for creating embeddings
-        '''
-        super(ProdFeatureEncoder, self).__init__()
+        """
+        Initializes the ProdFeatureEncoder model.
+
+        Args:
+            config (object): Configuration object containing model hyperparameters.
+        """
+        super().__init__()
         self.config = config
         self.tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny")
         self.model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
@@ -19,8 +30,15 @@ class ProdFeatureEncoder(nn.Module):
         self.norm = nn.LayerNorm(self.config.embedding_size)
 
     def forward(self, text: str):
-        t = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-        model_output = self.model(**{k: v.to(self.model.device) for k, v in t.items()})
+        """
+        Creates an embedding for the input text.
+        Args:
+            text (str): Input text to create an embedding for.
+        Returns:
+            torch.Tensor: Embedding vector for the input text.
+        """
+        tokens = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+        model_output = self.model(**{k: v.to(self.model.device) for k, v in tokens.items()})
         embedding = model_output.last_hidden_state[:, 0, :]
         embedding = self.fc(embedding)
         return embedding[0]
--- prod2vec/model/prod2vec_model.py
+++ prod2vec/model/prod2vec_model.py
@@ -1,20 +1,40 @@
-import torch.nn as nn
+import torch
+from torch import nn
 
-from encoder import ProdFeatureEncoder
+from model.encoder import ProdFeatureEncoder
 
 
 class Prod2VecModel(nn.Module):
+    """
+       A PyTorch model that combines image, attribute, name,
+       and description embeddings to produce a product embedding.
+
+       Attributes:
+           img_encoder (nn.Linear): Projects image embeddings to the embedding size.
+           attr_encoder (nn.Sequential): a ProdFeatureEncoder and a linear layer.
+           name_encoder (nn.Sequential): a ProdFeatureEncoder and a linear layer.
+           descr_encoder (nn.Sequential): a ProdFeatureEncoder and a linear layer.
+           bn (nn.BatchNorm1d): A batch normalization layer for the concatenated embeddings.
+    """
+
     def __init__(self, mode, config):
-        super(Prod2VecModel, self).__init__()
+        """
+            Args:
+            :param mode (str): The mode of the model, one of 'train', 'eval', or 'inference'.
+            :param config: A configuration object containing hyperparameters and settings.
+        """
+        super().__init__()
         self.mode = mode
         assert mode in ['train', 'eval', 'inference']
         self.config = config
 
-        self.img_encoder = nn.Linear(self.config.img_embedding_size, self.config.embedding_size)
+        self.img_encoder = nn.Linear(
+            self.config.img_embedding_size, self.config.embedding_size)
 
         self.attr_encoder = nn.Sequential(
             ProdFeatureEncoder(self.config),
-            nn.Linear(self.config.bert_output_size, self.config.embedding_size),
+            nn.Linear(self.config.bert_output_size,
+                      self.config.embedding_size),
         )
 
         self.name_encoder = nn.Sequential(
@@ -30,11 +50,29 @@ class Prod2VecModel(nn.Module):
         self.bn = nn.BatchNorm1d(self.embedding_size*4)
 
     def forward(self, img_embedding, name, descr, attr):
+        """
+            Forward pass of the model.
+
+            Args:
+                img_embedding (Tensor): The image embedding.
+                name (Tensor): The name feature.
+                descr (Tensor): The description feature.
+                attr (Tensor): The attribute feature.
+
+            Returns:
+                A tuple of five tensors:
+                    - img_embedding (Tensor): The projected image embedding.
+                    - attr_embedding (Tensor): The encoded attribute embedding.
+                    - name_embedding (Tensor): The encoded name embedding.
+                    - descr_embedding (Tensor): The encoded description embedding.
+                    - product_embedding (Tensor): The concatenated and normalized product embedding.
+        """
         img_embedding = self.img_encoder(img_embedding)
         attr_embedding = self.attr_encoder(attr)
         name_embedding = self.name_encoder(name)
         descr_embedding = self.descr_encoder(descr)
 
-        product_embedding = torch.cat((img_embedding, attr_embedding, name_embedding, descr_embedding), axis=1)
+        product_embedding = torch.cat(
+            (img_embedding, attr_embedding, name_embedding, descr_embedding), axis=1)
         product_embedding = self.bn(product_embedding)
         return img_embedding, attr_embedding, name_embedding, descr_embedding, product_embedding
